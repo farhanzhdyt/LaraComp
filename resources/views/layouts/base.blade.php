554c497f9189262d33400548e5acc0cafd46b014
farhanzhdyt <inzanePlayers1945@gmail.com>
2019-10-10 15:12:05 +0700

    footer

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -16,7 +16,7 @@
         {{-- stylesheets --}}
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/footer.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
         <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
         <link rel="stylesheet" href="{{ asset('css/hamburgers.css') }}">
 
@@ -84,8 +84,65 @@
         
         {{-- footer --}}
         <footer class="main-footer">
-            <div class="container text-center">
-                <p>&copy; <a href="">LARACOMP</a> 2019</p>
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-md-6 column-company-info">
+                        <h1 class="text-white">LARACOMP</h1>
+                        <p style="text-align: justify; width: 30rem;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+                    </div>
+                    <div class="col-md-6 column-company-links">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h2>Profil</h2>
+                                <ul>
+                                    <li>
+                                        <a class="nav-link" href="#about">Tentang</a>
+                                    </li>
+                                    <li>
+                                        <a class="nav-link" href="#service">Layanan</a>
+                                    </li>
+                                    <li>
+                                        <a class="nav-link" href="#">Produk</a>
+                                    </li>
+                                    <li>
+                                        <a class="nav-link" href="#">Harga</a>
+                                    </li>
+                                    <li>
+                                        <a class="nav-link" href="#">Team</a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h2>Kegiatan Umum</h2>
+                                <ul>
+                                    <li>
+                                        <a class="nav-link" href="{{ route('blog') }}">Blog</a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h2>Ekstra</h2>
+                                <ul>
+                                    <li>
+                                        <a class="nav-link" href="#">Testimonial</a>
+                                    </li>
+                                    <li>
+                                        <a class="nav-link" href="#">Karir</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="copyright mt-5">
+                    <p class="text-center text-white">&copy; <a href="#">LARACOMP</a> 2019</p>
+                </div>
             </div>
         </footer>
 
