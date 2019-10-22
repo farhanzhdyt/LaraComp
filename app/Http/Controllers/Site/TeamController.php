37a999d67240793ea0bde7de7ad0c94e412340ce
farhanzhdyt <inzanePlayers1945@gmail.com>
2019-10-22 12:45:08 +0700

    Backend:Team <DONE>

--- app/Http/Controllers/Site/TeamController.php
+++ app/Http/Controllers/Site/TeamController.php
@@ -8,25 +8,46 @@ use App\Team;
 
 class TeamController extends Controller
 {
+    public function __construct() 
+    {
+        $this->middleware('auth');
+    }
+
     public function index() 
     {
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
     	$teams = Team::paginate(15);
     	return view('site.team.index', compact("teams"));
     }
 
     public function show($id) 
     {
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
     	$teams = Team::findOrFail($id);
     	return view('site.team.show', compact("teams"));
     }
 
     public function create() 
     {
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
     	return view('site.team.create');
     }
 
     public function store(Request $request) 
     {
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
     	$msg = [
     		"required" => "Form tidak boleh kosong",
     		"max" => "Karakter maksimal 12"
@@ -56,12 +77,20 @@ class TeamController extends Controller
 
     public function edit($id) 
     {   
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
         $team = Team::findOrFail($id);
         return view('site.team.edit', compact("team"));
     }
 
     public function update(Request $request, $id) 
     {
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
         $msg = [
             "required" => "Form tidak boleh kosong",
             "max" => "Karakter maksimal 12"
@@ -91,9 +120,13 @@ class TeamController extends Controller
 
     public function destroy($id) 
     {
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
         $team = Team::findOrFail($id);
         $team->delete();
 
-        return redirect()->back()->with('success', 'Data Berhasil Dihapus!');
+        return redirect()->back()->with('success', 'Data successfully deleted!');
     }
 }
--- resources/views/message/flash-message.blade.php
+++ resources/views/message/flash-message.blade.php
@@ -1,38 +1,24 @@
 @if ($message = Session::get('success'))
-	<div class="alert alert-success alert-block">
-		<button type="button" class="close" data-dismiss="alert">×</button>	
-	        <strong>{{ $message }}</strong>
-	</div>
+	<div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-check"></i> Success!</h4>
+        {{ session('success') }}
+    </div>
 @endif
 
 
 @if ($message = Session::get('error'))
-	<div class="alert alert-danger alert-block">
-		<button type="button" class="close" data-dismiss="alert">×</button>	
-	        <strong>{{ $message }}</strong>
-	</div>
-@endif
-
-
-@if ($message = Session::get('warning'))
-	<div class="alert alert-warning alert-block">
-		<button type="button" class="close" data-dismiss="alert">×</button>	
-		<strong>{{ $message }}</strong>
-	</div>
-@endif
-
-
-@if ($message = Session::get('info'))
-	<div class="alert alert-info alert-block">
-		<button type="button" class="close" data-dismiss="alert">×</button>	
-		<strong>{{ $message }}</strong>
-	</div>
+	<div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-check"></i> Success!</h4>
+        {{ session('success') }}
+    </div>
 @endif
 
 
 @if ($errors->any())
-<div class="alert alert-danger">
-	<button type="button" class="close" data-dismiss="alert">×</button>	
-    Data gagal ditambah!
-</div>
+	<div class="alert alert-danger">
+		<button type="button" class="close" data-dismiss="alert">×</button>	
+	    Data gagal ditambah!
+	</div>
 @endif
