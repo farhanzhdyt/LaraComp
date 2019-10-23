a9d1d1bd85b0970c0bceb48f19a888d1278d0364
fikkry fachrezi <fikkryfachrezi25@gmail.com>
2019-10-23 16:27:41 +0700

    progress news management 30%

--- app/Http/Controllers/Site/NewsController.php
+++ app/Http/Controllers/Site/NewsController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Site;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\News;
 
 class NewsController extends Controller
 {
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +20,14 @@ class NewsController extends Controller
      */
     public function index()
     {
-        //
+        if  (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        } 
+
+
+        $news = News::all();
+
+        return view('site.news.index', compact('news'));
     }
 
     /**
@@ -24,7 +37,11 @@ class NewsController extends Controller
      */
     public function create()
     {
-        //
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
+        return view('site.news.create');
     }
 
     /**
@@ -35,7 +52,44 @@ class NewsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        } elseif (auth()->user()->level !== "ADMIN_BERITA") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
+        $this->validate($request, [
+            'title' => 'required|max:50|unique:news,title',
+            'description' => 'required|max:255',
+            'content' => 'required' ,
+            'image' => 'nullable|image|max:2040',
+        ]);
+
+        if($request->hasFile('image')) {
+
+            $fileNameWithExt = $request->file('image')->getClientOriginalName();
+
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+
+            $ext = $request->file('image')->getClientOriginalExtension();
+
+            $fileNameToStore = $fileName . '-' . rand() . '.' . $ext;
+
+            $path = $request->file('image')->move('images/news_images/', $fileNameToStore);
+        } else {
+            $fileNameToStore = "noimage.png";
+        }
+
+        $news = new News;
+        $news->title = $request->get('title');
+        $news->slug = Str::slug($request->input('title'));
+        $news->description = $request->get('description');
+        $news->content = $request->get('content');
+        $news->image = $fileNameToStore;
+        $news->created_by = auth()->user()->id;
+        $news->save();
+
+        return redirect()->route('news.index')->with('success', ' News successfully created');
     }
 
     /**
@@ -46,7 +100,15 @@ class NewsController extends Controller
      */
     public function show($id)
     {
-        //
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        } elseif (auth()->user()->level !== "ADMIN_BERITA") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
+        $news = News::findOrFail($id);
+
+        return view('site.news.show', compact('news'));
     }
 
     /**
@@ -57,7 +119,15 @@ class NewsController extends Controller
      */
     public function edit($id)
     {
-        //
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        } elseif (auth()->user()->level !== "ADMIN_BERITA") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
+        $news = News::findOrFail($id);
+
+        return view('site.news.edit', compact('news'));
     }
 
     /**
@@ -69,7 +139,45 @@ class NewsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        } elseif (auth()->user()->level !== "ADMIN_BERITA") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
+        $this->validate($request, [
+            'title' => 'required|max:50|unique:news,title',
+            'description' => 'required|max:255',
+            'content' => 'required' ,
+            'image' => 'nullable|image|max:2040',
+        ]);
+
+        if($request->hasFile('image')) {
+
+            $fileNameWithExt = $request->file('image')->getClientOriginalName();
+
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+
+            $ext = $request->file('image')->getClientOriginalExtension();
+
+            $fileNameToStore = $fileName . '-' . rand() . '.' . $ext;
+
+            $path = $request->file('image')->move('images/news_images/', $fileNameToStore);
+        } else {
+            $fileNameToStore = "noimage.png";
+        }
+
+        $news = News::findOrFail($id);
+
+        $news->title = $request->get('title');
+        $news->slug = Str::slug($request->input('title'));
+        $news->description = $request->get('description');
+        $news->content = $request->get('content');
+        $news->image = $fileNameToStore;
+        $news->created_by = auth()->user()->id;
+        $news->save();
+
+        return redirect()->route('news.index')->with('edit', 'News successfully update');
     }
 
     /**
@@ -80,6 +188,23 @@ class NewsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if (auth()->user()->level !== "ADMIN") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        } elseif (auth()->user()->level !== "ADMIN_BERITA") {
+            return redirect()->back()->with('error', 'Unauthorized Page');
+        }
+
+        $news = News::findOrFail($id);
+
+        $news->delete();
+
+        return redirect()->route('news.index')->with('delete', 'News Has Been Removed');
+    }
+
+    public function showClient($slug)
+    {
+        $news = News::findOrFail($slug);
+
+        return view('', compact('news'));
     }
 }
--- app/News.php
+++ app/News.php
@@ -3,8 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class News extends Model
 {
-    //
+    
+    use SoftDeletes;
+
+    protected $table = 'news';
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'content',
+        'image',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
